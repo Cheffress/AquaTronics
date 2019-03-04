cc3f6f6c3edb1f35c166ab564519e7860e188806
Cheffress <thejeffresssixdad@gmail.com>
2019-03-05 08:21:10 +1030

    Added RGB

--- Source/Solar Cycle.py
+++ Source/Solar Cycle.py
@@ -33,9 +33,21 @@ def logMessage(message):
 # Connects to local RPi
 pi = pp.pi()
 
-
+redLed = 17
+greenLed = 11
 blueLed = 26
+pi.set_mode(redLed, pp.OUTPUT)
+pi.set_mode(greenLed, pp.OUTPUT)
 pi.set_mode(blueLed, pp.OUTPUT)
+
+
+pi.write(redLed, True)
+pi.write(redLed, False)
+pi.write(greenLed, True)
+pi.write(greenLed, False)
+pi.write(blueLed, True)
+pi.write(blueLed, False)
+
 
 # Blinking LED
 if False:
@@ -56,7 +68,6 @@ if False:
 brightness = 1.0
 resolution = 0.01
 direction = 'asc'
-pi.set_PWM_dutycycle(blueLed, 255*brightness)
 while True:
     if direction == 'asc' and brightness == 1:
         direction = 'desc'
@@ -71,8 +82,10 @@ while True:
         brightness -= resolution
     
     # Apply new brightness
+    pi.set_PWM_dutycycle(redLed, 255*brightness)
+    pi.set_PWM_dutycycle(greenLed, 255*brightness)
     pi.set_PWM_dutycycle(blueLed, 255*brightness)
-    time.sleep(0.01)
+#    time.sleep(0.01)
 
 
 
