e2dad05f6d2f9ea0ddf801b2cb2ed32056006ff4
Cheffress <TheJeffressSixDad@gmail.com>
2019-12-23 15:44:20 +1030

    no message

--- Source/Solar Cycle.py
+++ Source/Solar Cycle.py
@@ -1,8 +1,32 @@
+#%%#########################################
+# Notes
+############################################
+# Create graph of colours throughout the day every 10 minutes
+
+
+
+
+#%%##############################
+# Import modules
+#################################
+
 import json
 import datetime as dt
 import time
 import pigpio as pp
+import tkinter
+import pandas as pd
+import os
+from datetime import datetime as dt
+import math
 
+piOutput = True
+host = 'pc'
+configFile = 'Config.json'
+
+rValue = 0
+gValue = 0
+bValue = 0
 # Must run below prior to running code
 # sudo pigpiod
 
@@ -10,10 +34,26 @@ import pigpio as pp
 #%%##############################
 # Initial setup
 #################################
+# Set curent working directory
+if host == 'pi':
+    homeDir = '/home/pi/Documents/Aquatronics/Source'
+else:
+    homeDir = 'E:/Git/Aquatronics/Source'
+
+os.chdir(homeDir)
+
 # Import config file
-configFile = '/home/pi/Documents/Aquatronics/Source/Config.json'
 CONFIG = json.loads(open(configFile,'r').read())
 
+
+#%%##############################
+# General setup
+#################################
+maxBrightness = CONFIG['led']['brightness']['max']
+minBrightness = CONFIG['led']['brightness']['min']
+resolution = CONFIG['led']['brightness']['resolution']
+sleepCycle = CONFIG['intervalSeconds']
+#schedule = CONFIG['schedule']
 
 
 #%%##############################
@@ -25,67 +65,122 @@ def logMessage(message):
     # Send to log file as well
 
 
+def readJson(fileName):
+    '''Import JSON file'''
+    json_output = json.loads(open(fileName,'r').read())
+    return json_output
+
+def timeInRange(start, end, x):
+    '''Return true if x is in the range [start, end]'''
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
+
+def hexToRgb(h):
+    return tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
+
+def ledColour(schedule,ledBrightness):
+    '''Returns rgb values'''
+    now = pd.to_datetime(dt.now())
+    
+    
+    r=1
+    g=2
+    b=3
+    return r, g, b
+
+#r,g,b = ledColour(schedule,CONFIG['led'])
+
+
+
+
+
+
+
+ledBrightness = CONFIG['led']['brightness']
+schedule = CONFIG['schedule']
+brightnessRange = ledBrightness['max'] - ledBrightness['min']
+
+schedule = pd.DataFrame(schedule)
+schedule['time'] = pd.to_datetime(schedule['time'], format='%H:%M%:%S', errors='ignore')
+endOfDay = pd.DataFrame([['23:59:59','000000']], columns=['time','colour'])
+schedule = schedule.append(endOfDay)
+schedule.sort_values(by='time')
+schedule['time'] = pd.to_datetime(schedule['time'])
+
+now = pd.to_datetime(dt.now())
+
+for i in range(1,len(schedule)):
+    if now >= schedule.iloc[i-1,0] and now < schedule.iloc[i,0]:
+        print(schedule.iloc[i-1,0],schedule.iloc[i,0])
+        periodSeconds = (schedule.iloc[i,0]-schedule.iloc[i-1,0]).total_seconds()
+        expiredSeconds = periodSeconds-(schedule.iloc[i,0]-now).total_seconds()
+        expiredPercent = expiredSeconds/periodSeconds
+        startRgb = hexToRgb(schedule.iloc[i-1,1])
+        endRgb = hexToRgb(schedule.iloc[i,1])
+        
+        
+        r = (abs(endRgb[0] - startRgb[0])/255 / ledBrightness['max'] * brightnessRange)+ledBrightness['min']
+        # Get r value
+        r = (startRgb[0]+(endRgb[0] - startRgb[0])*expiredPercent)
+        # Convert r value to within range
+        r = math.ceil(r/255*ledBrightness['max'])
+        
+        
+        
+        if startRgb <> endRgb:
+        
+        
+        
+        if r <= ledBrightness['min']:
+            # Turn r off
+            pi.write(redLed, False)
+        else:
+            pi.write(redLed, True)
+        
+        
+        
+        print(periodSeconds,expiredSeconds,expiredPercent)
+        break
+        
+    
+
+
+
+#%%##############################
+# Window setup
+#################################
+if piOutput == False:
+    window=tkinter.Tk()
+    window.title('Light colour display')
+    window.geometry('500x200')
+    window.configure(background='black')
+    window.mainloop()
+    
+
+
 
 #%%##############################
 # Board setup
 #################################
 
-# Connects to local RPi
-pi = pp.pi()
-
-redLed = 17
-greenLed = 11
-blueLed = 26
-pi.set_mode(redLed, pp.OUTPUT)
-pi.set_mode(greenLed, pp.OUTPUT)
-pi.set_mode(blueLed, pp.OUTPUT)
-
-
-pi.write(redLed, True)
-pi.write(redLed, False)
-pi.write(greenLed, True)
-pi.write(greenLed, False)
-pi.write(blueLed, True)
-pi.write(blueLed, False)
-
-
-# Blinking LED
-if False:
-    while True:
-        pi.write(blueLed, True)
-        logMessage('Blue LED On')
-        time.sleep(0.5)
-        pi.write(blueLed, False)
-        logMessage('Blue LED Off')
-        time.sleep(0.5)
-
-
-# Chaning brightness of LED
-
-
-# Dimming and brightening LED
-# Uses PWM
-maxBrightness = 100
-minBrightness = 7
-brightness = 100
-resolution = 1
-sleepCycle = 1
-direction = 'asc'
-while True:
-    if direction == 'asc' and brightness == maxBrightness:
-        direction = 'desc'
-        logMessage('Changing to descending')
-    if direction == 'desc' and brightness <= minBrightness:
-        direction = 'asc'
-        logMessage('Changing to ascending')
+if piOutput:
+    # Connects to local RPi
+    pi = pp.pi()
     
-    if direction == 'asc':
-        brightness += resolution
-    else:
-        brightness -= resolution
+    # Allocate pins and set mode
+    redLed = CONFIG['led']['pins']['r']
+    greenLed = CONFIG['led']['pins']['g']
+    blueLed = CONFIG['led']['pins']['b']
+    pi.set_mode(redLed, pp.OUTPUT)
+    pi.set_mode(greenLed, pp.OUTPUT)
+    pi.set_mode(blueLed, pp.OUTPUT)
     
-    # Apply new brightness
-    print(brightness)
+    
+    # Dimming and brightening LED
+    brightness = 0
+
     pi.set_PWM_dutycycle(redLed, brightness)
     pi.set_PWM_dutycycle(greenLed, brightness)
     pi.set_PWM_dutycycle(blueLed, brightness)
@@ -96,5 +191,6 @@ while True:
 #%%##############################
 # Board cleanup
 #################################
-pi.stop()
+if piOutput:
+    pi.stop()
 
